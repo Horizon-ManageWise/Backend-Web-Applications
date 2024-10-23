182c887ad7349da42b6c7929146c80d0cdf525b8
unknown <u20221d401@upc.edu.pe>
2024-10-23 13:19:16 -0500

    Primer commit: añadiendo archivos del proyecto existente

---
+++ src/main/java/pe/edu/upc/center/Backend_Web_Applications/BackendWebApplicationsApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.center.Backend_Web_Applications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWebApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWebApplicationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/center/Backend_Web_Applications/BackendWebApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.center.Backend_Web_Applications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWebApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
